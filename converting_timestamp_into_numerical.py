d7b0b1a8890524016abbcfbeb1080d0ec78b8b44
FINFOO22 <finnbfraser@gmail.com>
2025-08-13 16:18:09 +0100

    created the datetime_un_conversion() function for the next steps

--- converting_timestamp_into_numerical.py
+++ converting_timestamp_into_numerical.py
@@ -11,6 +11,16 @@ def datetime_conversion(datetime_string: str):
     seconds = [float(x) for x in split_[1].split('+')[0].split(':')]
     seconds = (seconds[0]*60*60 + seconds[1]*60 + seconds[2])
     print(f'datetime string: {datetime_string} | equivalent to: {split_[0]} at {seconds} seconds into the day')
+    return split_[0], seconds
+
+date, time = datetime_conversion(datetime_string=datetime)
 
 
-datetime_conversion(datetime_string=datetime)
+def datetime_un_conversion(time_seconds_float: float, date: str):
+    pass
+    time = ':'.join([('0' + str(x)) if x < 10 else str(x) for x in [int(time_seconds_float/3600), int((time_seconds_float%3600)/60), ((time_seconds_float%3600)%60)]])
+
+    datetime = 'T'.join([date, time])
+    print(f'Here is the datetime: {datetime}')
+
+datetime_un_conversion(time_seconds_float=time, date=date)
