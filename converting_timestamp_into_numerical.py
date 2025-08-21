85338a75ecb44ba3bb4604057b42ca53ba993bff
FINFOO22 <finnbfraser@gmail.com>
2025-08-21 11:47:29 +0100

    preparing to navigate calculating the sectors distances. I have mentioned some issues in the script testing_openf1_access,py

--- converting_timestamp_into_numerical.py
+++ converting_timestamp_into_numerical.py
@@ -10,10 +10,10 @@ def datetime_conversion(datetime_string: str):
     split_ = datetime_string.split('T')
     seconds = [float(x) for x in split_[1].split('+')[0].split(':')]
     seconds = (seconds[0]*60*60 + seconds[1]*60 + seconds[2])
-    print(f'datetime string: {datetime_string} | equivalent to: {split_[0]} at {seconds} seconds into the day')
+    # print(f'datetime string: {datetime_string} | equivalent to: {split_[0]} at {seconds} seconds into the day')
     return split_[0], seconds
 
-date, time = datetime_conversion(datetime_string=datetime)
+# date, time = datetime_conversion(datetime_string=datetime)
 
 
 def datetime_un_conversion(time_seconds_float: float, date: str):
@@ -21,6 +21,8 @@ def datetime_un_conversion(time_seconds_float: float, date: str):
     time = ':'.join([('0' + str(x)) if x < 10 else str(x) for x in [int(time_seconds_float/3600), int((time_seconds_float%3600)/60), ((time_seconds_float%3600)%60)]])
 
     datetime = 'T'.join([date, time])
-    print(f'Here is the datetime: {datetime}')
+    return datetime
 
-datetime_un_conversion(time_seconds_float=time, date=date)
+    #print(f'Here is the datetime: {datetime}')
+
+# datetime_un_conversion(time_seconds_float=time, date=date)
